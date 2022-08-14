65823eeedb32ffe32678b67dcfdac76fe67ac706
bimogempar <bimogempar@gmail.com>
2022-08-14 21:54:36 +0700

    first setup prisma

--- app/index.js
+++ app/index.js
@@ -1,11 +1,30 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Welcom bitchis!!');
+app.get('/', async (req, res) => {
+    const users = await prisma.User.findMany();
+    res.send(users);
 })
+
+app.post('/add', async (req, res) => {
+    const user = await prisma.User.create({
+        data: {
+            id: req.body.id,
+            name: req.body.name,
+            email: req.body.email,
+            address: req.body.address,
+        }
+    })
+    res.send(user)
+})
+
 
 module.exports = app
