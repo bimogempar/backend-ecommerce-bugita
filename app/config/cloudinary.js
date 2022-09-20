21fd48689cf508c465447cdb67b3924bc49c32e9
bimogempar <bimogempar@gmail.com>
2022-09-20 21:34:25 +0700

    update: delete cloudinary url config

--- app/config/cloudinary.js
+++ app/config/cloudinary.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 cloudinary.config({
-    cloudinary_url: process.env.CLOUDINARY_URL,
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
