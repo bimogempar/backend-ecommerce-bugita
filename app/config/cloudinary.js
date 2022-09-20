b27ca17483410a9e0c62992a0e844a5e5ea635a5
bimogempar <bimogempar@gmail.com>
2022-09-20 21:26:43 +0700

    update: add cloudinary url on config

--- app/config/cloudinary.js
+++ app/config/cloudinary.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 cloudinary.config({
+    cloudinary_url: process.env.CLOUDINARY_URL,
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
